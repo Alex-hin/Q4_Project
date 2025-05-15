157e37d6c5355bd5ecaf88729e6e4da7cc7c1667
Alex-hin <100032197@mvla.net>
2025-05-15 09:10:41 -0700

    added some finding logic

--- game/BoardGame.java
+++ game/BoardGame.java
@@ -2,10 +2,6 @@ package game;
 
 import java.util.*;
 import java.awt.*;
-import java.awt.Color;
-import java.awt.Graphics;
-
-
 
 // import for playing sound
 import javax.sound.sampled.AudioSystem;
@@ -20,18 +16,24 @@ public class BoardGame{
 
 	// Instance variables
     private ArrayList<Tile> bank, p1Tiles, currentPlayer, oldTiles;
+    private ArrayList<String> allWords;
     private Tile[][] board;
+    private int[][] newTiles;
+    private static int counter;
 
 	
     
     public BoardGame(){
 		p1Tiles = new ArrayList<Tile>();
         bank = new ArrayList<Tile>();
+        allWords = new ArrayList<String>();
         currentPlayer = p1Tiles;
         oldTiles = currentPlayer;
         board = new Tile[15][15];
+        newTiles = new int[2][7];
 
         readBank();
+        readWords();
         giveInitialTiles();
 	}
     public void giveInitialTiles() {
@@ -68,6 +70,20 @@ public class BoardGame{
             String line;
             while ((line = br.readLine()) != null) {
                 bank.add(new Tile(line.substring(0,1), Integer.parseInt(line.substring(2)), 0, 0, 40, 40));
+            }
+
+        } catch (IOException e){
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+
+    private void readWords(){
+        String filePath = "game/words.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                allWords.add(line);
             }
 
         } catch (IOException e){
@@ -127,6 +143,9 @@ public class BoardGame{
     public void playTile(int x, int y, Tile tile){
         if(board[x][y] == null){
             board[x][y] = tile;
+            newTiles[0][counter] = x;
+            newTiles[1][counter] = y;
+            counter++;
             currentPlayer.remove(tile);
         }
     }
@@ -143,4 +162,103 @@ public class BoardGame{
         }
     }
 
+    public boolean isValidPlay(){
+        if(counter == 0){
+            return false;
+        }
+
+        if(!isStraightLine()){
+            return false;
+        }
+
+        if(!isContinuous()){
+            return false;
+        }
+
+
+
+
+        return true;
+    }
+
+    private boolean isStraightLine(){
+        int newTileX = newTiles[0][0];
+        int NewTileY = newTiles[1][0];
+        for(int i = 1; i < newTiles.length; i++){
+            if(!(newTiles[0][i] == newTileX) || (newTiles[1][i] == NewTileY)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isContinuous() {
+        if (counter <= 1) {
+            return true;
+        }
+
+        if (newTiles[0][0] == newTiles[0][1]) {
+            int row = newTiles[0][0];
+            int[] cols = new int[counter];
+            for (int i = 0; i < counter; i++) {
+                cols[i] = newTiles[1][i];
+            }
+            
+            for (int i = 0; i < counter - 1; i++) {
+                for (int j = 0; j < counter - i - 1; j++) {
+                    if (cols[j] > cols[j + 1]) {
+                        int temp = cols[j];
+                        cols[j] = cols[j + 1];
+                        cols[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < counter - 1; i++) {
+                if (cols[i + 1] - cols[i] > 1) {
+                    for (int j = cols[i] + 1; j < cols[i + 1]; j++) {
+                        if (board[row][j] == null) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        } else if (newTiles[1][0] == newTiles[1][1]) {
+            int col = newTiles[1][0];
+            int[] rows = new int[counter];
+            for (int i = 0; i < counter; i++) {
+                rows[i] = newTiles[0][i];
+            }
+            for (int i = 0; i < counter - 1; i++) {
+                for (int j = 0; j < counter - i - 1; j++) {
+                    if (rows[j] > rows[j + 1]) {
+                        int temp = rows[j];
+                        rows[j] = rows[j + 1];
+                        rows[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < counter - 1; i++) {
+                if (rows[i + 1] - rows[i] > 1) {
+                    for (int j = rows[i] + 1; j < rows[i + 1]; j++) {
+                        if (board[j][col] == null) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void resetNewTileCounter() {
+        counter = 0;
+        newTiles = new int[2][7];
+    }
+
+    
 }
+
